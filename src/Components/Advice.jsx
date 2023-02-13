67c929cf49b6a28b6c9ffadf0829af3df6866ff0
RamiRbehi <ramirbehi92@gmail.com>
2023-02-13 21:53:25 +0100

    finish building app

--- src/Components/Advice.jsx
+++ src/Components/Advice.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../CSS/Advice.css"
 import divider from "../Images/pattern-divider-desktop.svg"
+import dividerMobile from "../Images/pattern-divider-mobile.svg"
 import dice from "../Images/icon-dice.svg" 
 
 const Advice = () => {
@@ -9,20 +10,32 @@ const Advice = () => {
     const [id, setId] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const handleClick = async () => {
-        setId(null);
-        setAdvice(null);
+    useEffect(() => {
+      const fetchData = async () => {
         setLoading(true);
         const res = await fetch("https://api.adviceslip.com/advice");
         const data = await res.json();
-        const {slip_id} = data.slip;
         const {advice} = data.slip;
+        const {id} = data.slip;
+        setAdvice(advice);
+        setId(id);
+        setLoading(false);
+      };
+      fetchData();
+    }, []);
+
+    const handleClick = async () => {
+        setAdvice(null);
+        setId(null);
+        setLoading(true);
+        const res = await fetch("https://api.adviceslip.com/advice");
+        const data = await res.json();
         setTimeout(() => {
-            setId(slip_id);
-            console.log(id);
-            setAdvice(advice);
-            console.log(advice);
-            setLoading(false);
+          const {advice} = data.slip;
+          const {id} = data.slip;
+          setAdvice(advice);
+          setId(id);
+          setLoading(false);
         }, 500);
     };
 
@@ -32,6 +45,7 @@ const Advice = () => {
         {loading && <p className="loading">Laoding...</p>}
         {advice && <p className="adviceText">"{advice}"</p>}
         <img src={divider} alt="" className="dividerDesktop" />
+        <img src={dividerMobile} alt="" className="dividerMobile" />
         <div className="btnDice" onClick={handleClick}>
         <img src={dice} alt="" className="dice" />
         </div>
